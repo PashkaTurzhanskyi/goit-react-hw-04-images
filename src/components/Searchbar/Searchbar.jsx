f110a081dd61ca1e1604f068ae44f18d46a51290
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-05-18 10:24:54 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -35,48 +35,6 @@ export const Searchbar = ({ onSubmit }) => {
   );
 };
 
-// export class Searchbar extends React.Component {
-//   state = {
-//     value: '',
-//   };
-
-//   handleInputChange = e => {
-//     this.setState({ value: e.target.value.toLowerCase() });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     if (this.state.value.trim() === '') {
-//       alert('Please enter a search query!');
-//     } else {
-//       this.props.onSubmit(this.state.value);
-//       // this.setState({ value: '' });
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <header className={css.Searchbar}>
-//         <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-//           <button type="submit" className={css.SearchFormButton}>
-//             <span className={css.SearchFormButtonLabel}>Search</span>
-//           </button>
-
-//           <input
-//             className={css.SearchFormInput}
-//             type="text"
-//             value={this.state.value}
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             onChange={this.handleInputChange}
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
-
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
